fb6ab2952b683310c735bc950499217c0470e794
Brice Tessier Huort <tessierhuort@gmail.com>
2016-09-13 11:28:16 +0200

    fixed navbar and implement routes messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :chatrooms
+  resources :chatrooms do
+    resource :chatroom_users #singular because it's just for yourself, our connection to a chatroom by itself
+                                #action create et delete de chatroom_users
+    resources :messages
+  end
+
 
   root to: 'chatrooms#index'
 end
